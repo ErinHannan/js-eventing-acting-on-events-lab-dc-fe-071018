b933363bcf94ab3f9eb9a56740011d046ff9fa74
ErinHannan <erinehannan@gmail.com>
2018-09-04 21:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var dodger = document.getElementById('dodger')
 
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(leftNumbers, 10)
+  var right = parseInt(rightNumbers, 10)
  
   if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
   
 document.addEventListener('keydown', function(e) {
